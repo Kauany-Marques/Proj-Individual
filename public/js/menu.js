72e67589ffbd16eb265a27f22263321798e0aa7c
kauany-marques <kauany.silva@sptech.school>
2025-05-17 11:11:33 -0300

    Atualizando index - htl, css e javascript

---
+++ public/js/menu.js
@@ -0,0 +1,20 @@
+
+var menu = document.querySelectorAll('.item-menu')
+//criei a var menu que guarda todos os .item-menu, ao invés de contar um por um
+
+
+// criei a função selecionarLink, com o for para percorrer o tam da lista.
+// peguei a lista na posição i (contador)
+function selecionarLink(){
+  for(var i = 0; i < menu.length; i++){
+    menu[i].classList.remove('ativo');
+  }
+    // vamos remover uma classe de um item (qualquer uma q quiser)
+
+    this.classList.add('ativo')
+// remove a classe ativo em um item q n cliquei
+//  e add class ativo e um q eu cliquei
+}
+for(var i = 0; i < menu.length; i++){
+    menu[i].addEventListener('click', selecionarLink);
+}
